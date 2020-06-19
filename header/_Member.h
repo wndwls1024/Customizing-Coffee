0e63e8754694e6a633406af031489375e13ec693
joon young <55422765+wndwls1024@users.noreply.github.com>
2020-06-20 04:55:41 +0900

    Create _Member.h

---
+++ header/_Member.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+#include <stddef.h>
+struct _Member
+{
+	string name = "";
+	string gender = "";
+	string phone = "";
+	string member_id = "";
+	string nickname = "";
+	int coupon = 0;
+	string level = "FRIEND";
+};
